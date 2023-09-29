88e2a8403a4f2124147b068b8ff48717d2640e4b
Alwaly <ndiayealwaly999@gmail.com>
2023-09-29 19:17:18 +0000

    Génerer le rapport de pandas profiling

--- src/process.py
+++ src/process.py
@@ -1,7 +1,10 @@
 from utils import *
+from pandas_profiling import ProfileReport
 
 def processor(file_name):
     data = csv_reader(file_name)
+    profile = ProfileReport(data)
+    profile.to_file(output_file='../docs/rapport.html')
     X, Y = process(data)
     X, X_, categorical_features, continuous_features = feature_engineering(X)
     X = normalisation(X, X_, categorical_features, continuous_features)
