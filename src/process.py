8eef07ffb9807c6352632f253faf9b98692d8b80
Alwaly <ndiayealwaly999@gmail.com>
2023-09-29 19:56:14 +0000

    Fix bug

--- src/process.py
+++ src/process.py
@@ -4,7 +4,7 @@ from pandas_profiling import ProfileReport
 def processor(file_name):
     data = csv_reader(file_name)
     profile = ProfileReport(data)
-    profile.to_file(output_file='docs/rapport.html')
+    profile.to_html('docs/rapport.html')
     X, Y = process(data)
     X, X_, categorical_features, continuous_features = feature_engineering(X)
     X = normalisation(X, X_, categorical_features, continuous_features)
